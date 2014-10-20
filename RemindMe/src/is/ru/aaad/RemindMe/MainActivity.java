0380b80010153bf58bbbcb0517f222c9ae6abfda
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-10-20 16:23:41 +0000

    a

---
+++ RemindMe/src/is/ru/aaad/RemindMe/MainActivity.java
@@ -0,0 +1,14 @@
+package is.ru.aaad.RemindMe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Johannes Gunnar Heidarsson on 15.10.2014.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- RemindMe/src/is/ru/aaad/RemindMe/SplashActivity.java
+++ RemindMe/src/is/ru/aaad/RemindMe/SplashActivity.java
@@ -1,7 +1,9 @@
 package is.ru.aaad.RemindMe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
     /**
@@ -10,6 +12,15 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
